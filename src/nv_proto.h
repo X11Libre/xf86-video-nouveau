X11Libre/xf86-video-nouveau

commit 794a27724fb9ab173dc7ea8062730797e1b4de04

nv50: use libwfb for pixmap access when driver pixmaps enabled

src/nv_proto.h
@@ -72,6 +72,12 @@ Bool nouveau_exa_init(ScreenPtr pScreen);
 Bool nouveau_exa_pixmap_is_onscreen(PixmapPtr pPixmap);
 bool nouveau_exa_pixmap_is_tiled(PixmapPtr ppix);
 
+/* in nouveau_wfb.c */
+void nouveau_wfb_setup_wrap(ReadMemoryProcPtr *, WriteMemoryProcPtr *,
+			    DrawablePtr);
+void nouveau_wfb_finish_wrap(DrawablePtr);
+void nouveau_wfb_init();
+
 /* in nv_hw.c */
 void NVCalcStateExt(ScrnInfoPtr,NVRegPtr,int,int,int,int,int,int);
 void NVLoadStateExt(ScrnInfoPtr pScrn,NVRegPtr);
@@ -271,6 +277,8 @@ int nv50_xv_port_attribute_get(ScrnInfoPtr, Atom, INT32 *, pointer);
 #ifndef exaMoveInPixmap
 extern void exaMoveInPixmap(PixmapPtr pPixmap);
 #endif
+
+extern Bool wfbPictureInit(ScreenPtr, PictFormatPtr, int);
 
 #endif /* __NV_PROTO_H__ */
 

src/Makefile.am
@@ -35,6 +35,7 @@ nouveau_drv_la_SOURCES = \
 			 nouveau_local.h \
 			 nouveau_ms.h \
 			 nouveau_exa.c nouveau_xv.c nouveau_dri2.c \
+			 nouveau_wfb.c \
 			 nv_accel_common.c \
 			 nv_bios.c \
 			 nv_const.h \

src/nouveau_exa.c
@@ -485,10 +485,12 @@ nouveau_exa_pixmap_is_tiled(PixmapPtr ppix)
 static void *
 nouveau_exa_pixmap_map(PixmapPtr ppix)
 {
+	ScrnInfoPtr pScrn = xf86Screens[ppix->drawable.pScreen->myNum];
+	NVPtr pNv = NVPTR(pScrn);
 	struct nouveau_bo *bo = nouveau_pixmap_bo(ppix);
 	unsigned delta = nouveau_pixmap_offset(ppix);
 
-	if (bo->tile_flags) {
+	if (bo->tile_flags && !pNv->wfb_enabled) {
 		struct nouveau_pixmap *nvpix = nouveau_pixmap(ppix);
 
 		nvpix->map_refcount++;
@@ -512,9 +514,11 @@ nouveau_exa_pixmap_map(PixmapPtr ppix)
 static void
 nouveau_exa_pixmap_unmap(PixmapPtr ppix)
 {
+	ScrnInfoPtr pScrn = xf86Screens[ppix->drawable.pScreen->myNum];
+	NVPtr pNv = NVPTR(pScrn);
 	struct nouveau_bo *bo = nouveau_pixmap_bo(ppix);
 
-	if (bo->tile_flags) {
+	if (bo->tile_flags && !pNv->wfb_enabled) {
 		struct nouveau_pixmap *nvpix = nouveau_pixmap(ppix);
 
 		if (--nvpix->map_refcount)

src/nouveau_wfb.c
@@ -1,0 +1,247 @@
+/*
+ * Copyright © 2009 Red Hat, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/* Authors:
+ *    Ben Skeggs <bskeggs@redhat.com>
+ */
+
+#include "nv_include.h"
+
+struct wfb_pixmap {
+	unsigned long base;
+	unsigned long end;
+	unsigned pitch;
+	unsigned horiz_tiles;
+	unsigned multiply_factor;
+};
+
+static struct wfb_pixmap wfb_pixmap[6];
+
+static inline FbBits
+nouveau_wfb_rd_linear(const void *src, int size)
+{
+	FbBits bits = 0;
+
+	memcpy(&bits, src, size);
+	return bits;
+}
+
+static inline void
+nouveau_wfb_wr_linear(void *dst, FbBits value, int size)
+{
+	memcpy(dst, &value, size);
+}
+
+#define TILE_FUNC_FOR_WRAP(name,wrap)                                          \
+static FbBits                                                                  \
+nouveau_wfb_rd_##name(const void *ptr, int size) {                             \
+	unsigned long offset = (unsigned long)ptr;                             \
+	struct wfb_pixmap *wfb = &wfb_pixmap[wrap];                            \
+	FbBits bits = 0;                                                       \
+	int x, y;                                                              \
+                                                                               \
+	offset -= wfb->base;                                                   \
+                                                                               \
+	y = (offset * wfb->multiply_factor) >> 32;                             \
+	x = offset - y * wfb->pitch;                                           \
+                                                                               \
+	offset  = (x >> TP) + ((y >> TH) * wfb->horiz_tiles);                  \
+	offset *= (1 << (TH + TP));                                            \
+	offset += ((y & THM) * (1 << TP)) + (x & TPM);                         \
+                                                                               \
+	memcpy(&bits, (void *)wfb->base + offset, size);                       \
+	return bits;                                                           \
+}                                                                              \
+                                                                               \
+static void                                                                    \
+nouveau_wfb_wr_##name(void *ptr, FbBits value, int size) {                     \
+	unsigned long offset = (unsigned long)ptr;                             \
+	struct wfb_pixmap *wfb = &wfb_pixmap[wrap];                            \
+	int x, y;                                                              \
+                                                                               \
+	offset -= wfb->base;                                                   \
+                                                                               \
+	y = (offset * wfb->multiply_factor) >> 32;                             \
+	x = offset - y * wfb->pitch;                                           \
+                                                                               \
+	offset  = (x >> TP) + ((y >> TH) * wfb->horiz_tiles);                  \
+	offset *= (1 << (TH + TP));                                            \
+	offset += ((y & THM) * (1 << TP)) + (x & TPM);                         \
+                                                                               \
+	memcpy((void *)wfb->base + offset, &value, size);                      \
+}
+
+#define TILE_FUNC(name)                                                        \
+	TILE_FUNC_FOR_WRAP(w00_##name, 0);                                     \
+	TILE_FUNC_FOR_WRAP(w01_##name, 1);                                     \
+	TILE_FUNC_FOR_WRAP(w02_##name, 2);                                     \
+	TILE_FUNC_FOR_WRAP(w03_##name, 3);                                     \
+	TILE_FUNC_FOR_WRAP(w04_##name, 4);                                     \
+	TILE_FUNC_FOR_WRAP(w05_##name, 5);                                     \
+	static struct {                                                        \
+		ReadMemoryProcPtr read;                                        \
+		WriteMemoryProcPtr write;                                      \
+	} nouveau_wfb_##name[6] = {                                            \
+		{ nouveau_wfb_rd_w00_##name, nouveau_wfb_wr_w00_##name },      \
+		{ nouveau_wfb_rd_w01_##name, nouveau_wfb_wr_w01_##name },      \
+		{ nouveau_wfb_rd_w02_##name, nouveau_wfb_wr_w02_##name },      \
+		{ nouveau_wfb_rd_w03_##name, nouveau_wfb_wr_w03_##name },      \
+		{ nouveau_wfb_rd_w04_##name, nouveau_wfb_wr_w04_##name },      \
+		{ nouveau_wfb_rd_w05_##name, nouveau_wfb_wr_w05_##name },      \
+	}
+
+#define HW_MODE 0
+#define TH (HW_MODE + 2)
+#define TP 5
+#define THM ((1 << TH) - 1)
+#define TPM ((1 << TP) - 1)
+TILE_FUNC(mode0);
+#undef TPM
+#undef THM
+#undef TP
+#undef TH
+#undef HW_MODE
+
+#define HW_MODE 1
+#define TH (HW_MODE + 2)
+#define TP 6
+#define THM ((1 << TH) - 1)
+#define TPM ((1 << TP) - 1)
+TILE_FUNC(mode1);
+#undef TPM
+#undef THM
+#undef TP
+#undef TH
+#undef HW_MODE
+
+#define HW_MODE 2
+#define TH (HW_MODE + 2)
+#define TP 6
+#define THM ((1 << TH) - 1)
+#define TPM ((1 << TP) - 1)
+TILE_FUNC(mode2);
+#undef TPM
+#undef THM
+#undef TP
+#undef TH
+#undef HW_MODE
+
+#define HW_MODE 3
+#define TH (HW_MODE + 2)
+#define TP 6
+#define THM ((1 << TH) - 1)
+#define TPM ((1 << TP) - 1)
+TILE_FUNC(mode3);
+#undef TPM
+#undef THM
+#undef TP
+#undef TH
+#undef HW_MODE
+
+#define HW_MODE 4
+#define TH (HW_MODE + 2)
+#define TP 6
+#define THM ((1 << TH) - 1)
+#define TPM ((1 << TP) - 1)
+TILE_FUNC(mode4);
+#undef TPM
+#undef THM
+#undef TP
+#undef TH
+#undef HW_MODE
+
+void
+nouveau_wfb_setup_wrap(ReadMemoryProcPtr *pRead, WriteMemoryProcPtr *pWrite,
+		       DrawablePtr pDraw)
+{
+	struct nouveau_pixmap *nvpix;
+	struct wfb_pixmap *wfb;
+	PixmapPtr ppix;
+	int wrap;
+
+	if (!pRead || !pWrite)
+		return;
+
+	ppix = NVGetDrawablePixmap(pDraw);
+	if (!ppix)
+		return;
+	nvpix = nouveau_pixmap(ppix);
+
+	wrap = 0;
+	while (wfb_pixmap[wrap].base)
+		wrap++;
+	wfb = &wfb_pixmap[wrap];
+
+	wfb->base = (unsigned long)ppix->devPrivate.ptr;
+	wfb->end = wfb->base + nvpix->bo->size;
+	wfb->pitch = ppix->devKind;
+	wfb->multiply_factor = (0xFFFFFFFF / wfb->pitch) + 1;
+	wfb->horiz_tiles = wfb->pitch / (nvpix->bo->tile_mode ? 64 : 32);
+
+	if (!nvpix->bo->tile_flags) {
+		*pRead = nouveau_wfb_rd_linear;
+		*pWrite = nouveau_wfb_wr_linear;
+		return;
+	}
+
+	switch (nvpix->bo->tile_mode) {
+	case 0:
+		*pRead = nouveau_wfb_mode0[wrap].read;
+		*pWrite = nouveau_wfb_mode0[wrap].write;
+		break;
+	case 1:
+		*pRead = nouveau_wfb_mode1[wrap].read;
+		*pWrite = nouveau_wfb_mode1[wrap].write;
+		break;
+	case 2:
+		*pRead = nouveau_wfb_mode2[wrap].read;
+		*pWrite = nouveau_wfb_mode2[wrap].write;
+		break;
+	case 3:
+		*pRead = nouveau_wfb_mode3[wrap].read;
+		*pWrite = nouveau_wfb_mode3[wrap].write;
+		break;
+	case 4:
+		*pRead = nouveau_wfb_mode4[wrap].read;
+		*pWrite = nouveau_wfb_mode4[wrap].write;
+		break;
+	default:
+		FatalError("uh oh\n");
+	}
+}
+
+void
+nouveau_wfb_finish_wrap(DrawablePtr pDraw)
+{
+	struct wfb_pixmap *wfb = &wfb_pixmap[0];
+	PixmapPtr ppix;
+
+	ppix = NVGetDrawablePixmap(pDraw);
+	if (!ppix)
+		return;
+
+	while (wfb->base != (unsigned long)ppix->devPrivate.ptr)
+		wfb++;
+	wfb->base = 0;
+}
+

src/nv_driver.c
@@ -1128,8 +1128,13 @@ NVPreInit(ScrnInfoPtr pScrn, int flags)
 			"Using \"Shadow Framebuffer\" - acceleration disabled\n");
 	}
 
-	if (xf86ReturnOptValBool(pNv->Options, OPTION_EXA_PIXMAPS, FALSE))
+	if (xf86ReturnOptValBool(pNv->Options, OPTION_EXA_PIXMAPS, FALSE)) {
 		pNv->exa_driver_pixmaps = TRUE;
+#if XORG_VERSION_CURRENT >= XORG_VERSION_NUMERIC(1,6,99,0,0)
+		if (pNv->Architecture >= NV_50)
+			pNv->wfb_enabled = TRUE;
+#endif
+	}
 
 	if(xf86GetOptValInteger(pNv->Options, OPTION_VIDEO_KEY, &(pNv->videoKey))) {
 		xf86DrvMsg(pScrn->scrnIndex, X_CONFIG, "video key set to 0x%x\n",
@@ -1395,6 +1400,11 @@ NVPreInit(ScrnInfoPtr pScrn, int flags)
 	 * XXX This should be taken into account in some way in the mode valdation
 	 * section.
 	 */
+
+	if (pNv->wfb_enabled) {
+		if (xf86LoadSubModule(pScrn, "wfb") == NULL)
+			NVPreInitFail("\n");
+	}
 
 	if (xf86LoadSubModule(pScrn, "fb") == NULL)
 		NVPreInitFail("\n");
@@ -2008,18 +2018,26 @@ NVScreenInit(int scrnIndex, ScreenPtr pScreen, int argc, char **argv)
 	}
 
 	switch (pScrn->bitsPerPixel) {
-		case 16:
-		case 32:
-			ret = fbScreenInit(pScreen, FBStart, pScrn->virtualX, pScrn->virtualY,
-				pScrn->xDpi, pScrn->yDpi,
-				displayWidth, pScrn->bitsPerPixel);
-			break;
-		default:
-			xf86DrvMsg(scrnIndex, X_ERROR,
-				"Internal error: invalid bpp (%d) in NVScreenInit\n",
-				pScrn->bitsPerPixel);
-			ret = FALSE;
-			break;
+	case 16:
+	case 32:
+	if (pNv->wfb_enabled) {
+		ret = wfbScreenInit(pScreen, FBStart, pScrn->virtualX,
+				    pScrn->virtualY, pScrn->xDpi, pScrn->yDpi,
+				    displayWidth, pScrn->bitsPerPixel,
+				    nouveau_wfb_setup_wrap,
+				    nouveau_wfb_finish_wrap);
+	} else {
+		ret = fbScreenInit(pScreen, FBStart, pScrn->virtualX,
+				   pScrn->virtualY, pScrn->xDpi, pScrn->yDpi,
+				   displayWidth, pScrn->bitsPerPixel);
+	}
+		break;
+	default:
+		xf86DrvMsg(scrnIndex, X_ERROR,
+			   "Internal error: invalid bpp (%d) in NVScreenInit\n",
+			   pScrn->bitsPerPixel);
+		ret = FALSE;
+		break;
 	}
 	if (!ret)
 		return FALSE;
@@ -2037,7 +2055,10 @@ NVScreenInit(int scrnIndex, ScreenPtr pScreen, int argc, char **argv)
 		}
 	}
 
-	fbPictureInit (pScreen, 0, 0);
+	if (pNv->wfb_enabled)
+		wfbPictureInit (pScreen, 0, 0);
+	else
+		fbPictureInit (pScreen, 0, 0);
 
 	xf86SetBlackWhitePixels(pScreen);
 

src/nv_type.h
@@ -202,6 +202,7 @@ typedef struct _NVRec {
     uint8_t cur_head;
     ExaDriverPtr	EXADriverPtr;
     Bool		exa_driver_pixmaps;
+    Bool		wfb_enabled;
     ScreenBlockHandlerProcPtr BlockHandler;
     CloseScreenProcPtr  CloseScreen;
     /* Cursor */

commit eb93278fadaab508ba8c2db65d2d3af2bd421efa

wfb: Remove declaration for undefined function nouveau_wfb_init()
Fixes: 794a277 ("nv50: use libwfb for pixmap access when driver pixmaps enabled")
Signed-off-by: Rhys Kidd <rhyskidd@gmail.com>

src/nv_proto.h
@@ -61,7 +61,6 @@ Bool NVAccelM2MF(NVPtr pNv, int w, int h, int cpp, uint32_t srco, uint32_t dsto,
 void nouveau_wfb_setup_wrap(ReadMemoryProcPtr *, WriteMemoryProcPtr *,
 			    DrawablePtr);
 void nouveau_wfb_finish_wrap(DrawablePtr);
-void nouveau_wfb_init();
 
 /* in nv_shadow.c */
 void NVRefreshArea(ScrnInfoPtr pScrn, int num, BoxPtr pbox);