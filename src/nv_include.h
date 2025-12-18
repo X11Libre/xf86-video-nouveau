f94d45667bea872ef2d551d9f98581df8bfa18f1
Enrico Weigelt, metux IT consult <info@metux.net>
2025-12-18 12:03:00 +0100

    drop support for ancient pre-7.1 xextproto
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- src/nv_include.h
+++ src/nv_include.h
@@ -48,13 +48,7 @@
 #include "xf86DDC.h"
 
 #include <X11/extensions/randr.h>
-
-#ifdef HAVE_XEXTPROTO_71
 #include <X11/extensions/dpmsconst.h>
-#else
-#define DPMS_SERVER
-#include <X11/extensions/dpms.h>
-#endif
 
 #define NV_DMA_DEBUG 0
 
