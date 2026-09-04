3247edb67b632be3c18587a982817cb725e64d5f
Enrico Weigelt, metux IT consult <info@metux.net>
2026-09-04 12:30:12 +0200

    ci: bump action-build-driver to v0.4.0

---
+++ src/nouveau_present.h
@@ -0,0 +1,17 @@
+#ifndef __NOUVEAU_PRESENT_H__
+#define __NOUVEAU_PRESENT_H__
+
+#include "xorg-server.h"
+#include "scrnintstr.h"
+
+#include "config.h"
+
+#if defined(DRI3)
+#include "present.h"
+Bool nouveau_present_init(ScreenPtr pScreen);
+void nouveau_present_fini(ScreenPtr pScreen);
+#else
+static inline Bool nouveau_present_init(ScreenPtr pScreen) { return FALSE; }
+static inline void nouveau_present_fini(ScreenPtr pScreen) { }
+#endif
+#endif
